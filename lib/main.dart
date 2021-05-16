3a3a9b51c99c25a02502eca47860a69738dd8242
FlutLab.io <>
2021-05-16 23:26:19 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,33 +9,36 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final String title = 'Candra Julius Sinaga';
-  int number = 0;
-
-  void tekanTombol() {
-    setState(() {
-      number = number + 1;
-    });
-  }
+  String message = "Ini adalah text";
+  String messageButton = "Tekan kamu";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text(title),
-            ),
-            body: Center(
-                child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Text(
-                number.toString(),
-                style: new TextStyle(fontSize: 10 + number.toDouble()),
-              ),
-              RaisedButton(child: Text('Tambah Bilangan'), onPressed: tekanTombol)
-            ]))));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Center(
+              child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Text(message),
+            RaisedButton(
+              child: Text(messageButton),
+              onPressed: () {
+                setState(() {
+                  message = "Tombol sudah ditekan";
+                });
+              },
+            )
+          ]))),
+    );
   }
 }
 
 // kesimpulan dari Codingan
 /*
-   
+   Annonymus method adalah sebuah method dengan tidak memiliki nama
+   Kalau method yang memiliki nama bukan annonymus method
+   Cara membuat Annonymus method dengan (){ }
+   Cara membuat bukan Annonymus method dengan void candra(){ }
   */
